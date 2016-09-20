452da7416884aa63a830246195b7f6bbf2ab1b47
cilenedv <cilenedelacruz@gmail.com>
2016-09-20 10:26:03 -0500

    Subiendo cambios de la nueva version 0.02

--- js/app.js
+++ js/app.js
@@ -1,14 +1,30 @@
 window.addEventListener("load", function() {
-		var boton=document.getElementById("boton");
-		boton.addEventListener("click",function(){
-			var txt=document.getElementById("txt").value;
-			document.getElementById("txt").value="";
-			var mensaje=document.createElement("p");
-			mensaje.innerText=txt;
-			var contenedor=document.getElementById("contenedor");
-			contenedor.insertBefore(mensaje,contenedor.childNodes[0]);
-	});
-
+    var boton = document.getElementById("boton");
+    var textArea = document.getElementById("texto");
+    var contador=document.getElementById("contador");
+    var caracteres = contador.innerHTML=140;
+    boton.addEventListener("click", function(e) {
+        e.preventDefault();
+        var texto = textArea.value;
+        agregarMensaje(texto);
+        textArea.value = "";
+        contador.innerHTML=140;
+        boton.disabled=true;
+    });
+    function agregarMensaje(texto) {
+        var mensaje = document.createElement("div");
+        mensaje.innerText = texto;
+        var contenedor = document.getElementById("contenedor");
+        contenedor.insertBefore(mensaje, contenedor.childNodes[0]);
+    }
+    texto.addEventListener("keydown", function() {
+        boton.disabled = false;
+        var longitud = textArea.value.length;
+        if(longitud <= caracteres) {
+           contador.innerHTML = caracteres - longitud;
+        } else {
+            contador.innerHTML = caracteres - longitud;
+        }
+    });
+  
 });
-
-
--- readme.md
+++ readme.md
@@ -1,7 +1,11 @@
 #Crear un contenedor vacio que se pueda mandar mensajes y se acumule a la vez en la misma pantalla
 
-- En el html
+ En .html
 - Crear un form dentro de ello crear un textarea y un button.
 - Al textarea se le pondra un type"texto" y un id "txt".
 - Al button se le dara type"button" y un id "boton".
 - Después craer un div y le pondremos un id"contenedor" fuera del form vendria hacer un nodo hermano de from.
+
+
+En .js
+- creamos un --> window.addEventListener("load", function() para que cargue la función de allí declaramos la variable boton.
